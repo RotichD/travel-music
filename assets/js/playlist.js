61d2526ba0fda581f69377b789de0b1c7f40f3f2
4FRO <drotich@ucsc.edu>
2021-04-28 20:11:43 -0700

    random number for playlist

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -1,10 +1,12 @@
+var playlistNumber = 0;
+
 // Returns a random playlist and embeds the player in HTML
 
 // Playlist Embedded Codes Arrays for each weather condition
 
 
 // if clear or temp > 70 then random related playlists
-var warmSongs = [];
+var warmSongs = ["sunny", "sunny test", "sunnnnny test"];
 
 
 // if clear and temp > 80 then random related playlists
@@ -20,3 +22,13 @@ var dustySongs = [];
 
 // if tornado or squall
 var chaoticSongs = [];
+
+// determines which random playlist to insert depending on the length of the arrary (how many playlists available)
+var getRandomInt = function(size) {
+    
+    playlistNumber = Math.floor(Math.random() * size);
+
+;}
+
+
+
