1fd2f635c995d4b0dad07bed855704f3153cc1bf
4FRO <drotich@ucsc.edu>
2021-04-28 20:00:23 -0700

    basic direction

---
+++ assets/js/playlist.js
@@ -0,0 +1,22 @@
+// Returns a random playlist and embeds the player in HTML
+
+// Playlist Embedded Codes Arrays for each weather condition
+
+
+// if clear or temp > 70 then random related playlists
+var warmSongs = [];
+
+
+// if clear and temp > 80 then random related playlists
+var hotSongs = [];
+
+
+// switch statement if rain or drizzle or fog or cloudy or mist 
+var chillSongs = [];
+
+
+// switch statement if smoke or haze or dust or sand or ash
+var dustySongs = []; 
+
+// if tornado or squall
+var chaoticSongs = [];
