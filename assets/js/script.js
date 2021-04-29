6f8c058abf0541ba4269449e136c30a9e42769d3
Abigail <asexton5@nd.edu>
2021-04-28 22:59:39 -0400

    elements weather js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 // elements
+var today = new Date();
+var cityFormEl = document.querySelector("#city-form");
+var cityNameInputEl = document.querySelector("#cityname");
+var currentWeatherEl = document.querySelector('#current-weather');
+var currentWeatherCardEl = document.querySelector("#current-weather-card")
 
 
-// variables
+
 
 
