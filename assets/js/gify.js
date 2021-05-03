bce13c3acadc5224b520a5465bdde51820796dd9
4FRO <drotich@ucsc.edu>
2021-05-03 16:50:30 -0700

    correctly displays gif

--- assets/js/gify.js
+++ assets/js/gify.js
@@ -1,41 +1,17 @@
 /* All of this is dependent on the weather API search result */
-function getGif() {
-    fetch(
-      // Make a fetch request to openWeather to get weather for location
-      
-    )
-      .then(function(weatherResponse) {
-        return weatherResponse.json();
-      })
-      .then(function(weatherResponse) {
-        // Create a variable to hold the desitnation weather
-        var searchTerm = weatherResponse.query.location;//unsure what this will look like //this variable needs to be in the fetch for giphy
-  
-        // Display the article title above the GIF as a <h2> heading
-        var responseHeaderEl = document.querySelector('#response-header');
-        responseHeaderEl.innerHTML = '<h2>' + searchTerm + '</h2>';
+function getGif(searchTerm) {
   
         // Return a fetch request to the Giphy search API with the article title and rating parameters
-        return fetch(
-          'https://api.giphy.com/v1/gifs/search?q=' +
-            searchTerm +
-            '&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&limit=1'
-        );
-      })
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(response) {
-        if (response.data.length === 0) {
-          console.log('Giphy could not find anything for that.');
-        } else {
-          console.log(response.data[0]);
-          var weatherImgContainerEl = document.querySelector('#weather-img');
-          weatherImgContainerEl.innerHTML = '';
-          var weatherImg = document.createElement('img');
-          weatherImg.setAttribute('src', response.data[0].images.fixed_height.url);
-          weatherImg.className = "pure-img"; //Pure CSS class for responsive image sizing
-          weatherImgContainerEl.appendChild(weatherImg);
-        }
-      });
+        fetch('https://api.giphy.com/v1/gifs/search?q=' + searchTerm + '&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&limit=1').then(function(response) {
+          response.json().then(function(gify) {
+            if (gify.data.length === 0) {
+              console.log('Giphy could not find anything for that.');
+            } else {
+              console.log(gify.data[0]);
+              var weatherImgContainerEl = document.querySelector('#weather-img');
+              weatherImgContainerEl.innerHTML = '<img src="' + gify.data[0].images.fixed_height.url + '" class="pure-img" />'; // changes image based on search term
+              
+            }
+          });
+      })   
 }
--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -87,7 +87,22 @@ var insertPlaylist = function (weatherCondition) {
   spotifyPlayer.src = url_string;
 };
 
-
+function getGif(searchTerm) {
+  
+  // Return a fetch request to the Giphy search API with the article title and rating parameters
+  fetch('https://api.giphy.com/v1/gifs/search?q=' + searchTerm + '&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&limit=1').then(function(response) {
+    response.json().then(function(gify) {
+      if (gify.data.length === 0) {
+        console.log('Giphy could not find anything for that.');
+      } else {
+        console.log(gify.data[0]);
+        var weatherImgContainerEl = document.querySelector('#weather-img');
+        weatherImgContainerEl.innerHTML = '<img src="' + gify.data[0].images.fixed_height.url + '" class="pure-img" />'; // changes image based on search term
+        
+      }
+    });
+})   
+}
 
 // decides what array to use based on weather api
 
@@ -105,7 +120,7 @@ var displayWeatherAndMusic = function() {
 
       var musicTemp = data.main.temp;
       
-
+      getGif(data.weather[0].description);
       
       switch(data.weather[0].main) {
         case 'Clear':
